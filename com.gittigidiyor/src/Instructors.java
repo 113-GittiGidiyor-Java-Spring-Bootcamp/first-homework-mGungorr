083ee67d6708b377b55fd6138f3ea47cdbc99e36
Mustafa Güngör <mustafa.gungor@felece.com>
2021-08-13 15:07:15 +0300

    constructors and getter,setters added

--- com.gittigidiyor/src/Instructors.java
+++ com.gittigidiyor/src/Instructors.java
@@ -1,5 +1,62 @@
+import java.util.Objects;
+
 public class Instructors {
     private String instructorName;
     private String instructorAdress;
     private long instructorPhone;
+
+    public Instructors(String instructorName, String instructorAdress, long instructorPhone) {
+        this.instructorName = instructorName;
+        this.instructorAdress = instructorAdress;
+        this.instructorPhone = instructorPhone;
+    }
+
+    public Instructors() {
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public String getInstructorAdress() {
+        return instructorAdress;
+    }
+
+    public void setInstructorAdress(String instructorAdress) {
+        this.instructorAdress = instructorAdress;
+    }
+
+    public long getInstructorPhone() {
+        return instructorPhone;
+    }
+
+    public void setInstructorPhone(long instructorPhone) {
+        this.instructorPhone = instructorPhone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Instructors that = (Instructors) o;
+        return instructorPhone == that.instructorPhone && Objects.equals(instructorName, that.instructorName) && Objects.equals(instructorAdress, that.instructorAdress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(instructorName, instructorAdress, instructorPhone);
+    }
+
+    @Override
+    public String toString() {
+        return "Instructors{" +
+                "instructorName='" + instructorName + '\'' +
+                ", instructorAdress='" + instructorAdress + '\'' +
+                ", instructorPhone=" + instructorPhone +
+                '}';
+    }
 }
--- com.gittigidiyor/src/Student.java
+++ com.gittigidiyor/src/Student.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.Objects;
 
 public class Student {
     private String name;
@@ -6,4 +7,68 @@ public class Student {
     private String adress;
     private Gender gender;
 
+    public Student() {
+    }
+
+    public Student(String name, Date birthDate, String adress, Gender gender) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.adress = adress;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(birthDate, student.birthDate) && Objects.equals(adress, student.adress) && gender == student.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, birthDate, adress, gender);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", adress='" + adress + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
 }
